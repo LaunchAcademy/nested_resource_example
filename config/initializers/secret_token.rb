1bd9078987bc5e96a4651b07448eef4ecc239540
Dan Pickett <dan.pickett@launchacademy.com>
2013-12-31 13:34:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NestedResource::Application.config.secret_key_base = 'a6974de13b93dfb05869c5d4dff300e6e167414a95990d2e6dc06bb3d1a354fcb8fbc9d4bf0ef0903074bf151b8233f086eb0de0513306ee40d5ac36179193f4'
