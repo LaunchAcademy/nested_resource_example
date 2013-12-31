654fd2593a6827c81ec262bc8779b3894167406a
Dan Pickett <dan.pickett@launchware.com>
2013-12-31 14:26:52 -0500

    handle redirection after category creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ NestedResource::Application.routes.draw do
   namespace :admin do
     resources :categories
   end
+
+  root 'homes#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
