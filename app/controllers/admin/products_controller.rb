e822e270848214c7f631f68e6a6548fb33ad8447
Dan Pickett <dan.pickett@launchware.com>
2013-12-31 14:38:19 -0500

    the need for a products#create action emerges

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,5 +3,19 @@ module Admin
     def new
       @product = Product.new
     end
+
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        redirect_to root_path, notice: 'Product created.'
+      else
+        render 'new'
+      end
+    end
+
+    protected
+    def product_params
+      params.require(:product).permit(:category_id, :name)
+    end
   end
 end
