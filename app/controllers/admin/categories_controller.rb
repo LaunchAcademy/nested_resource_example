0d34d29db19fd3a768c958e9bc42361b78f1b7c0
Dan Pickett <dan.pickett@launchware.com>
2013-12-31 14:25:07 -0500

    create action for creating categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,5 +3,19 @@ module Admin
     def new
       @category = Category.new
     end
+
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    end
+
+    protected
+    def category_params
+      params.require(:category).permit(:name)
+    end
   end
 end
